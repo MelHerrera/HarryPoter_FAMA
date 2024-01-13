6efc7f6f32ab3975e838dcff027bbb38aa75d21f
Melkin Herrera mendoza <melkinantonioh@gmail.com>
2024-01-13 14:03:50 -0600

    Initial commit

---
+++ app/src/main/java/com/example/harrypoter_fama/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.harrypoter_fama
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
